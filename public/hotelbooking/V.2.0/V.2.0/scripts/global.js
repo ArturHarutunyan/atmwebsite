67985ac3aa68a23aebcd9c27f588dedf45a80eff
ArturHarutunyan <artur.harutunyan1999@gmail.com>
2019-07-30 11:55:19 +0000

    .

---
+++ public/hotelbooking/V.2.0/V.2.0/scripts/global.js
@@ -0,0 +1,35 @@
+
+var touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
+
+var touchendEvent = 'ontouchstart' in window ? 'touchend' : 'click';
+
+var hotelDateP = [...document.querySelectorAll('.datepicker_input')];
+
+
+var carRentPrice = 125;
+
+var userOrders = [];
+var cloneOrders = [];
+
+
+
+
+var carPrices = [20,25]
+
+
+
+var rooms ={
+    'ani plaza hotel': [65,80],
+    'minotel barsam suites': [62,72],
+    'best western plus congress hotel': [72,95],
+    'metropol hotel': [65,72]
+
+}
+var roomsIndex = [
+    'ani plaza hotel',
+    'minotel barsam suites',
+    'best western plus congress hotel',
+    'metropol hotel'
+]
+
+var valuta = '$'
