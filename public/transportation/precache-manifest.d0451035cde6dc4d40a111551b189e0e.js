b7d5a68b365ceedb9a725987c309d67f7ac1a653
Mherdv <mherdv@gmail.com>
2019-09-23 13:20:11 +0400

    Routes fix

---
+++ public/transportation/precache-manifest.d0451035cde6dc4d40a111551b189e0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e259d5f057dc2a2cda88e84dcab5c046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43377ccfdfef63f57ed8",
+    "url": "/static/css/main.fc680cdd.chunk.css"
+  },
+  {
+    "revision": "f4ece7020cb8a1d002f5",
+    "url": "/static/js/2.af14c417.chunk.js"
+  },
+  {
+    "revision": "43377ccfdfef63f57ed8",
+    "url": "/static/js/main.fbfa6d17.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
