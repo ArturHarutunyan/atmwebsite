7533eb2bd9530217912927a4757ba3a71c77b728
Mherdv <mherdv@gmail.com>
2019-11-26 12:22:09 +0400

    timeout before adding

--- resources/views/layouts/site_inner.blade.php
+++ resources/views/layouts/site_inner.blade.php
@@ -266,20 +266,28 @@ $types=TourType::all();
 </div>
 @yield('after_scripts')
 <script>
-        window.fbAsyncInit = function() {
-            FB.init({
-                xfbml            : true,
-                version          : 'v5.0'
-            });
-        };
 
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    </script>
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    setTimeout(()=>{
+
+        window.fbAsyncInit = function() {
+                FB.init({
+                    xfbml            : true,
+                    version          : 'v5.0'
+                });
+            };
+    
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+      },1000);
+})
+
+</script>
 </body>
 </html>
